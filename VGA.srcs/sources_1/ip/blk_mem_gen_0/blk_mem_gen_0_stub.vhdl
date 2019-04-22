0de99de21fd4c68566e81d01e2b9e54ef0403bdf
Jacob Boline <mail@Jboline.us>
2019-04-21 21:35:58 -0500

    Shuckle Test Image

--- VGA.srcs/sources_1/ip/blk_mem_gen_0/blk_mem_gen_0_stub.vhdl
+++ VGA.srcs/sources_1/ip/blk_mem_gen_0/blk_mem_gen_0_stub.vhdl
@@ -1,7 +1,7 @@
 -- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
--- Date        : Sat Apr 20 22:03:45 2019
+-- Date        : Sun Apr 21 21:17:06 2019
 -- Host        : DESKTOP-KBPHQS1 running 64-bit major release  (build 9200)
 -- Command     : write_vhdl -force -mode synth_stub
 --               C:/Users/xtjac/School/VGA-Controller/VGA.srcs/sources_1/ip/blk_mem_gen_0/blk_mem_gen_0_stub.vhdl
--- rtl/VGA.sv
+++ rtl/VGA.sv
@@ -38,16 +38,104 @@ end
 
 always_comb begin
     if(dispValid_q) begin
-        if(data == 0) begin
-            VGA_R = 0;
-            VGA_G = 0;
-            VGA_B = 0;
-        end
-        else begin
-            VGA_R = 15;
-            VGA_G = 15;
-            VGA_B = 15;
-        end
+        case(data)
+            0:
+                begin
+                    VGA_R = 4'hf;
+                    VGA_G = 4'hf;
+                    VGA_B = 4'hf;
+                end
+            1:
+                begin
+                    VGA_R = 4'he;
+                    VGA_G = 4'hf;
+                    VGA_B = 4'hf;
+                end
+            2:
+                begin
+                    VGA_R = 4'hf;
+                    VGA_G = 4'hf;
+                    VGA_B = 4'hc;
+                end
+            3:
+                begin
+                    VGA_R = 4'hf;
+                    VGA_G = 4'he;
+                    VGA_B = 4'hb;
+                end
+            4:
+                begin
+                    VGA_R = 4'hf;
+                    VGA_G = 4'hd;
+                    VGA_B = 4'h7;
+                end
+            5:
+                begin
+                    VGA_R = 4'hd;
+                    VGA_G = 4'hd;
+                    VGA_B = 4'ha;
+                end
+            6:
+                begin
+                    VGA_R = 4'hf;
+                    VGA_G = 4'hd;
+                    VGA_B = 4'h4;
+                end
+            7:
+                begin
+                    VGA_R = 4'hf;
+                    VGA_G = 4'hc;
+                    VGA_B = 4'h4;
+                end
+            8:
+                begin
+                    VGA_R = 4'hf;
+                    VGA_G = 4'hc;
+                    VGA_B = 4'h3;
+                end
+            9:
+                begin
+                    VGA_R = 4'he;
+                    VGA_G = 4'hc;
+                    VGA_B = 4'h4;
+                end
+            10:
+                begin
+                    VGA_R = 4'hc;
+                    VGA_G = 4'hb;
+                    VGA_B = 4'h8;
+                end
+            11:
+                begin
+                    VGA_R = 4'hd;
+                    VGA_G = 4'h9;
+                    VGA_B = 4'h5;
+                end
+            12:
+                begin
+                    VGA_R = 4'hc;
+                    VGA_G = 4'h5;
+                    VGA_B = 4'h4;
+                end
+            13:
+                begin
+                    VGA_R = 4'h8;
+                    VGA_G = 4'h7;
+                    VGA_B = 4'h4;
+                end
+            14:
+                begin
+                    VGA_R = 4'ha;
+                    VGA_G = 4'h3;
+                    VGA_B = 4'h2;
+                end
+            15:
+                begin
+                    VGA_R = 4'h1;
+                    VGA_G = 4'h1;
+                    VGA_B = 4'h1;
+                end
+        endcase // data
     end
     else begin
         VGA_R = 0;
