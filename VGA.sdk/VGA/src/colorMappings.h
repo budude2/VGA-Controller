946e2495bf815b26a878551f857a452b0351f484
Jacob Boline <mail@Jboline.us>
2019-05-10 22:31:59 -0500

    Fix spacing

--- VGA.sdk/VGA/src/colorMappings.h
+++ VGA.sdk/VGA/src/colorMappings.h
@@ -12,7 +12,7 @@
 #define BLUE	 		0x1
 #define GREEN	 		0x2
 #define CYAN     		0x3
-#define RED		 		0x4
+#define RED             0x4
 #define MAGENTA	 		0x5
 #define BROWN	 		0x6
 #define GRAY			0x7
@@ -21,7 +21,7 @@
 #define	BRIGHTGREEN		0xA
 #define BRIGHTCYAN		0xB
 #define BRIGHTRED		0xC
-#define BRIGHTMAGENTA	0xD
+#define BRIGHTMAGENTA   0xD
 #define YELLOW			0xE
 #define WHITE			0xF
 
