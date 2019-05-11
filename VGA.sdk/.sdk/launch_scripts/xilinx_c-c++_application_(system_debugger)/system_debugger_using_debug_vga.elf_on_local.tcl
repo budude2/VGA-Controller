3c3717f4aae6bd23f33a5f18dc849746552cca3f
Jacob Boline <mail@Jboline.us>
2019-05-10 22:29:45 -0500

    Remove unneeded folders

--- VGA.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_vga.elf_on_local.tcl
+++
@@ -1,8 +0,0 @@
-connect -url tcp:127.0.0.1:3121
-configparams mdm-detect-bscan-mask 2
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292A4BA2CA"} -index 0
-rst -processor
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292A4BA2CA"} -index 0
-dow C:/Users/Jacob/School/VGA-Controller/VGA.sdk/VGA/Debug/VGA.elf
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys4DDR 210292A4BA2CA"} -index 0
-con
