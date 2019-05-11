d6f547a947afe61e08c3f629d82322ad4b327efc
Jacob Boline <mail@Jboline.us>
2019-05-10 22:32:42 -0500

    Convert tabs to spaces.

--- VGA.sdk/VGA/src/colorMappings.h
+++ VGA.sdk/VGA/src/colorMappings.h
@@ -8,21 +8,21 @@
 #ifndef SRC_COLORMAPPINGS_H_
 #define SRC_COLORMAPPINGS_H_
 
-#define BLACK			0x0
-#define BLUE	 		0x1
-#define GREEN	 		0x2
-#define CYAN     		0x3
+#define BLACK           0x0
+#define BLUE            0x1
+#define GREEN           0x2
+#define CYAN            0x3
 #define RED             0x4
-#define MAGENTA	 		0x5
-#define BROWN	 		0x6
-#define GRAY			0x7
-#define DARKGRAY		0x8
-#define BRIGHTBLUE		0x9
-#define	BRIGHTGREEN		0xA
-#define BRIGHTCYAN		0xB
-#define BRIGHTRED		0xC
+#define MAGENTA         0x5
+#define BROWN           0x6
+#define GRAY            0x7
+#define DARKGRAY        0x8
+#define BRIGHTBLUE      0x9
+#define BRIGHTGREEN     0xA
+#define BRIGHTCYAN      0xB
+#define BRIGHTRED       0xC
 #define BRIGHTMAGENTA   0xD
-#define YELLOW			0xE
-#define WHITE			0xF
+#define YELLOW          0xE
+#define WHITE           0xF
 
 #endif /* SRC_COLORMAPPINGS_H_ */
